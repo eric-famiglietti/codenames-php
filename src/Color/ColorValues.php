894ab333e29ff20abd628020c3807d0df37bd30b
Eric Famiglietti <eric.famiglietti@gmail.com>
2018-07-16 22:30:01 -0400

    Add Color class.

---
+++ src/Color/ColorValues.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Codenames\Color;
+
+use Codenames\Enumeration;
+
+abstract class ColorValues extends Enumeration
+{
+    /** @var int */
+    const RED = 0;
+
+    /** @var int */
+    const BLUE = 1;
+}
